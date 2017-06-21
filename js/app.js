e91f5e9c8a71045efc5d7c3c8111bac4af650eeb
asdisa <asde3211q@gmail.com>
2017-06-21 17:44:54 +0400

    enemy basic fields implimented

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -41,6 +44,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
+    console.log("up");
     player.handleInput(allowedKeys[e.keyCode]);
 });
